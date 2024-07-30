1da55343fac5b0a0a70c952521294dfc5f14c451
XavierZambrano <xavierzamco@gmail.com>
2024-07-30 16:10:00 -0600

    feat: add query argument

--- google_maps_scraper/spiders/text_search.py
+++ google_maps_scraper/spiders/text_search.py
@@ -6,5 +6,14 @@ class TextSearchSpider(scrapy.Spider):
     allowed_domains = ["google.com"]
     start_urls = ["https://google.com"]
 
+    def __init__(self, query='', *args, **kwargs):
+        super(TextSearchSpider, self).__init__(*args, **kwargs)
+
+        if not query:
+            raise ValueError('query is required')
+
+        query = query.replace(' ', '+')
+        self.start_urls = [f'https://google.com/search/{query}/']
+
     def parse(self, response):
         pass
