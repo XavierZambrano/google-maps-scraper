1f12fa28fb54b75b74c2284f12773cff556efc72
XavierZambrano <xavierzamco@gmail.com>
2024-07-30 19:00:22 -0600

    feat: parse data

--- google_maps_scraper/settings.py
+++ google_maps_scraper/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "google_maps_scraper.spiders"
 #USER_AGENT = "google_maps_scraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- google_maps_scraper/spiders/text_search.py
+++ google_maps_scraper/spiders/text_search.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 
 
 class TextSearchSpider(scrapy.Spider):
@@ -13,7 +14,49 @@ class TextSearchSpider(scrapy.Spider):
             raise ValueError('query is required')
 
         query = query.replace(' ', '+')
-        self.start_urls = [f'https://google.com/search/{query}/']
+        self.start_urls = [f'https://google.com/maps/search/{query}/']
 
     def parse(self, response):
-        pass
+        script = response.xpath('//script[contains(text(), "window.APP_INITIALIZATION_STATE")]/text()').get()
+        initialization_state = script.split('window.APP_INITIALIZATION_STATE=')[1].split('];')[0] + ']'
+        initialization_state_json = json.loads(initialization_state)
+        data1 = initialization_state_json[3]
+        data2 = data1[2]
+
+        data3 = json.loads(data2[5:])
+
+        # skip the first one, it's other data [1:]
+        for index, place_data in enumerate(data3[0][1][1:]):
+            data4 = place_data[14]
+
+            cid = data3[16][3][0][4][index][0][1]
+
+            _id = data4[78]
+            types = data4[13]
+            primaryType = types[0]
+            nationalPhoneNumber = data4[178][0][1][1][1]
+            internationalPhoneNumber = data4[178][0][0]
+            formattedAddress = data4[18]
+            location = {
+                'latitude': data4[9][2],
+                'longitude': data4[9][3],
+            }
+            rating = data4[4][7]
+            googleMapsUri = 'https://maps.google.com/?cid=' + cid
+            displayName = {
+                'text': data4[11],
+            }
+
+            yield {
+                'id': _id,
+                'primaryType': primaryType,
+                'nationalPhoneNumber': nationalPhoneNumber,
+                'internationalPhoneNumber': internationalPhoneNumber,
+                'formattedAddress': formattedAddress,
+                'location': location,
+                'rating': rating,
+                'googleMapsUri': googleMapsUri,
+                'displayName': displayName,
+            }
+
+
