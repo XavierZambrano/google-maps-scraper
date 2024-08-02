a30df10e99edfc88447290b215c785751c999718
XavierZambrano <xavierzamco@gmail.com>
2024-08-02 02:48:02 -0600

    feat: change the endpoint to https://www.google.com/search, it is the true endpoint, https://www.google.com/maps/search (what it was using) what does is call https://www.google.com/search

--- google_maps_scraper/spiders/text_search.py
+++ google_maps_scraper/spiders/text_search.py
@@ -11,26 +11,37 @@ class TextSearchSpider(scrapy.Spider):
     name = "text_search"
     allowed_domains = ["google.com"]
 
-    def __init__(self, query='', language='en', *args, **kwargs):
+    def __init__(self, query='', language='en', gl='us', max_results=20, *args, **kwargs):
         super(TextSearchSpider, self).__init__(*args, **kwargs)
+
+        max_results = int(max_results)
 
         if not query:
             raise ValueError('query is required')
 
         if language not in GOOGLE_SUPPORTED_LANGUAGES.values():
             raise ValueError(f'language {language} is not supported, please use one of: {json.dumps(GOOGLE_SUPPORTED_LANGUAGES, indent=2)}')
-
+        # TODO check if gl is supported
+        # if gl not in ...
+        if not (20 <= max_results <= 120 and max_results % 20 == 0):
+            raise ValueError('max_results must be between 20 and 120 and multiple of 20')
         query = query.replace(' ', '+')
-        self.start_urls = [f'https://www.google.com/maps/search/?api=1&query={query}&hl={language}']
+        page = 1
+        start_url = f'https://www.google.com/search?tbm=map&authuser=0&hl={language}&q={query}&tch=1&ech={page}'
+        if gl:
+            start_url += f'&gl={gl}'
+        self.start_urls = [start_url]
 
     def parse(self, response):
-        script = response.xpath('//script[contains(text(), "window.APP_INITIALIZATION_STATE")]/text()').get()
-        initialization_state = script.split('window.APP_INITIALIZATION_STATE=')[1].split('];')[0] + ']'
-        initialization_state_json = json.loads(initialization_state)
-        data1 = initialization_state_json[3]
-        data2 = data1[2]
-
-        data3 = json.loads(data2[5:])
+        # TODO working on this...
+        # script = response.xpath('//script[contains(text(), "window.APP_INITIALIZATION_STATE")]/text()').get()
+        # initialization_state = script.split('window.APP_INITIALIZATION_STATE=')[1].split('];')[0] + ']'
+        # initialization_state_json = json.loads(initialization_state)
+        # data1 = initialization_state_json[3]
+        # data2 = data1[2]
+        #
+        # data3 = json.loads(data2[5:])
+        data3 = json.loads(response.text.replace('\\n', '').replace('\\', '')[16:].split('","e":')[0])
 
         # skip the first one, it's other data [1:]
         for index, place_data in enumerate(data3[0][1][1:]):
